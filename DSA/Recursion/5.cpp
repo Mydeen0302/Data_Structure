9db97526aa95e6710cfa709be21f35dd03759194
Mydeen0302 <mydeen0302@gmail.com>
2024-02-13 17:47:16 +0530

    updated

---
+++ DSA/Recursion/5.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool checkpalindromme(int i, string &s)
+{
+    if(i>s.size()/2){
+        return true;
+    }
+    else{
+        if(s[i]!= s[s.size()-i-1]){
+            return false;
+        
+        }
+        else{
+               return checkpalindromme(i+1,s);
+        }
+
+    }
+
+}
+int fibbonacciOfN(int n)
+{
+    if(n<=1)
+    {
+    return n;
+    }
+    else{
+        return fibbonacciOfN(n-1)+fibbonacciOfN(n-2);
+    }
+}
+int main() {
+  string s="mdam";
+
+  cout<<boolalpha<<checkpalindromme(0,s);
+
+  int n;
+
+  cin>>n;
+  cout<<fibbonacciOfN(n);
+    return 0;
+}
