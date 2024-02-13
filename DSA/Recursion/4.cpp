c3c7b4eb5a1922dbe1ab6ee25e2082a033ffd118
Mydeen0302 <mydeen0302@gmail.com>
2024-02-13 13:39:35 +0530

    added reverse array 2

--- DSA/Recursion/4.cpp
+++ DSA/Recursion/4.cpp
@@ -9,7 +9,16 @@ void revarr(int l, int r, int arr[]) {
         revarr(l + 1, r - 1, arr);
     }
 }
-
+void revarr2(int i,int n,int arr[])
+{
+    if(i>=n/2){
+        return ;
+    }
+    else{
+        swap(arr[i],arr[n-i-1]);
+        revarr2(i+1,n,arr);
+    }
+}
 
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
@@ -19,6 +28,10 @@ int main() {
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
-
+    cout<<endl;
+    revarr2(0,n,arr);
+      for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
